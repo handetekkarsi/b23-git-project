318f69ed07ab5e16f93f1bbd71b5fae3c6807e89
handetekkarsi <tekkarsihande@gmail.com>
2021-07-19 23:20:58 +0300

    added some new commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello git world");
+        System.out.println("Adding awesome codes here");
     }
 }
